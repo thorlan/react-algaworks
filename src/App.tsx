5f30249a84cbf4dc587840de16931b8f7e7e1840
Thiago Orlandini <thorlandini@gmail.com>
2021-07-02 16:46:00 -0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HelloWorld/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/HelloWorld.tsx
@@ -0,0 +1,10 @@
+function HelloWorld () {
+
+    const name = "Thiago";
+
+    return <div>
+        <h1>Hello, { name } !</h1>
+    </div>
+}
+
+export default HelloWorld;
